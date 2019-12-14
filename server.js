a9470c100cff9cd61cc0d306cf24d9c81a5b7193
MichaelE1 <mickel13@gmail.com>
2019-12-14 12:04:39 +1000

    change to unique ids

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const sqlite3 = require("sqlite3").verbose();
+const shortid = require("shortid");
 const PORT = 3000;
 const app = express();
 app.use(express.json());
@@ -13,14 +14,16 @@ const db = new sqlite3.Database(dbFile);
 // If db file does not exist, create it
 db.serialize(() => {
   if (!fs.existsSync(dbFile)) {
-    db.run("CREATE TABLE Cards (id INTEGER PRIMARY KEY, content TEXT)");
+    db.run(
+      "CREATE TABLE Cards (id INTEGER PRIMARY KEY, shortid STRING, content TEXT)"
+    );
     console.log("Cards table created!");
   }
 });
 
 app.post("/access", (req, res) => {
   db.get(
-    "SELECT id, content FROM Cards WHERE id = ?",
+    "SELECT shortid, content FROM Cards WHERE shortid = ?",
     [req.body.ID.toString()],
     (err, row) => {
       if (err) console.log(err);
@@ -30,12 +33,13 @@ app.post("/access", (req, res) => {
 });
 
 app.post("/save", (req, res) => {
+  const id = shortid.generate();
   db.run(
-    "INSERT INTO Cards (content) VALUES (?)",
-    [JSON.stringify(req.body)],
+    "INSERT INTO Cards (shortid, content) VALUES (?, ?)",
+    [id, JSON.stringify(req.body)],
     function(err) {
       if (err) console.log(err);
-      res.json({ id: this.lastID });
+      res.json({ id });
     }
   );
 });
